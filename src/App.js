b5315367dd2ba031e3dd3561be770a22f9bcd73d
MohammadAbuQattam <mj.abuqattam@gmail.com>
2024-05-18 11:30:40 +0300

    add a change

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          hi <code>src/App.js</code> and ss to reload.
+          our dump CS Advisory Bot
         </p>
         <a
           className="App-link"
