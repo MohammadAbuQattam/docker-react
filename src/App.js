dd269b471714a7d1aa55339628156a2452f5caca
MohammadAbuQattam <mj.abuqattam@gmail.com>
2024-05-18 12:43:41 +0300

    add api calling

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    fetch('http://my-backend-env.eba-tpn3mzj5.eu-west-1.elasticbeanstalk.com/')
+      .then(response => response.json())
+      .then(data => setMessage(data.message))
+      .catch(error => console.error('Error fetching the message:', error));
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          our dump CS Advisory Bot (:
+          {message || 'Loading...'}
         </p>
         <a
           className="App-link"
